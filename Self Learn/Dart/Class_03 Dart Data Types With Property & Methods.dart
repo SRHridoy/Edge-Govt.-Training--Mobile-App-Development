83487ae2b54067f21e99341d71cf96638238aee9
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2024-11-27 14:09:33 +0600

    revisiting dart concepets//

--- Self Learn/Dart/Class_03 Dart Data Types With Property & Methods.dart
+++ Self Learn/Dart/Class_03 Dart Data Types With Property & Methods.dart
@@ -1,4 +1,4 @@
-main() {
+void main() {
   //String
   String myName = 'Md. Sohanur Rahman Hridoy!';
   String herName = "Mst. Nafiza Khanom Nova";
@@ -15,6 +15,7 @@ main() {
   print(str.replaceAll('#', ''));
 
   print(str);
+  //Remove unnecessary whitespaces...
   print(str.trim());
   print(str.trimLeft());
   print(str.trimRight());
@@ -33,19 +34,72 @@ main() {
   print(age.isFinite);
 
   print(age);
+  //check data type :
   print(age.runtimeType);
 
   String strAge = '21';
   print(strAge);
-
+  //int to string :
   age = int.parse(strAge);
   print(age.runtimeType);
 
   //Boolean
-  
+  bool isTrue = true;
+  print(isTrue);
+  bool isFact = 2 > 5;
+  print(isFact);
+
+  //Double :
+  double balance = 500.55040;
+  print(balance);
+  print(balance.toInt());
+  print(balance.toString());
+  print(balance.toStringAsFixed(3));
+
   //List
+  List ageList = [10, 11, 31, 33, 22];
+  print(ageList.length);
+  print(ageList.isEmpty);
+  print(ageList.isNotEmpty);
+  print(ageList.runtimeType);
+  print(ageList);
+  print(ageList.first);
+  print(ageList[1]);
+  print(ageList.indexOf(22));
+  //if missing show -1
+  print(ageList.indexOf(111));
+
+  List list = [12, true, false, 5.22, 'hi', 'eeps'];
+  print(list.indexOf('hi'));
+
+  //Suffling :
+  print(list);
+  list.shuffle();
+  print(list);
+  //Reverse :
+  print(ageList.reversed);
+
+  list.add(44);
+  print(list);
+
+  List updatedList = [33, 2, 1, 55, 6, 7, 32];
+  list.addAll(updatedList);
+  list.insert(4, 553);
+  list.insertAll(3, updatedList);
+  print(list);
+
+  List numList = [1, 2, 4, 4, 3, 5];
+  numList.remove(4);
+  print(numList);
+  numList.removeAt(0);
+  print(numList);
+  numList.removeLast();
+  print(numList);
+  numList.removeRange(0, numList.length);
+  print(numList);
 
   //Map
+  
 
   //Set
 
