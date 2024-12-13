a01394aa67602b2e15b660ad4e66295fe2de8bc6
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2024-12-13 20:00:53 +0600

    Completed the note app and clear concept about SQLFLITE CREATE,INSERT,UPDATE,DELETE...;

--- BootCamp Classes/notes_app/lib/data/local/db_helper.dart
+++ BootCamp Classes/notes_app/lib/data/local/db_helper.dart
@@ -67,4 +67,41 @@ class DBHelper{
     return mData;
   }
 
+
+  ///Update data:
+
+  Future<bool> updateNote({required String title, required String desc, required int sNo})async{
+    var db = await getDB();
+//     db.execute('''
+//   UPDATE $TABLE_NOTE
+//   SET $COLUMN_NOTE_TITLE = ?, $COLUMN_NOTE_DESCRIPTION = ?
+//   WHERE $COLUMN_NOTE_SNO = ?
+// ''', [title, desc, sNo]);
+//
+//     // You may check the number of affected rows with a separate query
+//     int rowsEffected = Sqflite.firstIntValue(await db.rawQuery('''
+//     SELECT changes() AS affectedRows
+//   ''')) ?? 0;
+
+
+    int rowsEffected = await db.update(TABLE_NOTE, {
+      COLUMN_NOTE_TITLE:title,
+      COLUMN_NOTE_DESCRIPTION:desc,
+    },where: "$COLUMN_NOTE_SNO = $sNo");
+
+    return rowsEffected>0;
+  }
+
+  /// delete note:
+
+  Future<bool> deleteNote({required int sNo})async{
+    var db = await getDB();
+
+    //db.delete(TABLE_NOTE,where: "$COLUMN_NOTE_SNO = $sNo");
+    int rowsEffected = await db.delete(TABLE_NOTE,where: "$COLUMN_NOTE_SNO = ?", whereArgs: ['$sNo']);
+
+    return rowsEffected>0;
+  }
+
+
 }
--- BootCamp Classes/notes_app/lib/pages/home_page.dart
+++ BootCamp Classes/notes_app/lib/pages/home_page.dart
@@ -35,103 +35,50 @@ class _HomPageState extends State<HomePage> {
           leading: Text('${allNotes[index][DBHelper.COLUMN_NOTE_SNO]}'),
           title: Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
           subtitle: Text(allNotes[index][DBHelper.COLUMN_NOTE_DESCRIPTION]),
+          trailing: SizedBox(
+            width: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              InkWell(
+                  onTap: () {
+                    // Update the controllers with the selected note data
+                    titleController.text = allNotes[index][DBHelper.COLUMN_NOTE_TITLE];
+                    descriptionController.text = allNotes[index][DBHelper.COLUMN_NOTE_DESCRIPTION];
+
+                    // Open the bottom sheet
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return getBottomSheetWidget(
+                          isUpdate: true,
+                          sNo: allNotes[index][DBHelper.COLUMN_NOTE_SNO],
+                        );
+                      },
+                    );
+                  },
+
+                  child: Icon(Icons.edit)),
+              InkWell(
+                  onTap: () async{
+                    bool check = await dbRef!.deleteNote(sNo: allNotes[index][DBHelper.COLUMN_NOTE_SNO]);
+                    if(check){
+                      getNotes();
+                    }
+                  },
+                  child: Icon(Icons.delete,color: Colors.red,)),
+            ],),
+          ),
         );
       },itemCount: allNotes.length,):Center(
         child: Text('No Notes noted!'),
       ),
 
       floatingActionButton: FloatingActionButton(onPressed: () async{
-        String errorMessage ='*Please fill the required fields!';
         /// note to be added from here:
         showModalBottomSheet(context: context, builder: (context) {
-          
-          return Container(
 
-            decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
-            ),
-
-            child: Padding(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                children: [
-                  Text('Add Note',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)
-                  ),
-                  SizedBox(height: 21,),
-                  TextField(
-                    controller: titleController,
-                   decoration: InputDecoration(
-                     hintText: 'Enter title here',
-                     label: Text('Text *'),
-                     focusedBorder: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(11),
-                     ),enabledBorder: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(11),
-                     ),
-                   ),
-                  ),
-                  SizedBox(height: 11),
-                  TextField(
-                    controller: descriptionController,
-                   maxLines: 4,
-                   decoration: InputDecoration(
-                     hintText: 'Enter description here',
-                     label: Text('Desc *'),
-                     focusedBorder: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(11),
-                     ),enabledBorder: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(11),
-                     ),
-                   ),
-                  ),
-                  SizedBox(height: 21,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            padding: EdgeInsets.all(15),
-                            side: BorderSide(
-                              width: 1
-                            )
-                          ),
-                            onPressed: () async{
-                            var title = titleController.text;
-                            var desc = descriptionController.text;
-                            if(title.isNotEmpty && desc.isNotEmpty){
-                              bool isAdded = await dbRef!.addNote(mTitle: title, mDesc: desc);
-                              if(isAdded){
-                                getNotes();
-                              }
-                            }else{
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(errorMessage)));
-                            }
-                            titleController.clear();
-                            descriptionController.clear();
-                            Navigator.pop(context);
-
-                        }, child: Text('Add Note',style: TextStyle(color: Colors.green),)),
-                      ),
-                      SizedBox(width: 11),
-                      Expanded(
-                        child: OutlinedButton(
-                            style: OutlinedButton.styleFrom(
-                              padding: EdgeInsets.all(15),
-                                side: BorderSide(
-                                    width: 1
-                                )
-                            ),
-                            onPressed: () {
-                              Navigator.pop(context);
-                        }, child: Text('Cancel',style: TextStyle(color: Colors.red),)),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          );
+          return getBottomSheetWidget();
         },);
       },child: Icon(Icons.add),),
 
@@ -145,5 +92,93 @@ class _HomPageState extends State<HomePage> {
     });
   }
 
+  Widget getBottomSheetWidget({bool isUpdate = false,int sNo = 0}){
+    String errorMessage ='*Please fill the required fields!';
+    return Container(
 
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
+      ),
+
+      child: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Text(isUpdate? 'Update Note':'Add Note',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)
+            ),
+            SizedBox(height: 21,),
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                hintText: 'Enter title here',
+                label: Text('Text *'),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(11),
+                ),enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(11),
+              ),
+              ),
+            ),
+            SizedBox(height: 11),
+            TextField(
+              controller: descriptionController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: 'Enter description here',
+                label: Text('Desc *'),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(11),
+                ),enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(11),
+              ),
+              ),
+            ),
+            SizedBox(height: 21,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.all(15),
+                          side: BorderSide(
+                              width: 1
+                          )
+                      ),
+                      onPressed: () async{
+                        var title = titleController.text;
+                        var desc = descriptionController.text;
+                        if(title.isNotEmpty && desc.isNotEmpty){
+                          bool isAdded = isUpdate? await dbRef!.updateNote(title: title, desc: desc, sNo: sNo):await dbRef!.addNote(mTitle: title, mDesc: desc);
+                          if(isAdded){
+                            getNotes();
+                          }
+                        }else{
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(errorMessage)));
+                        }
+                        titleController.clear();
+                        descriptionController.clear();
+                        Navigator.pop(context);
+                      }, child: Text(isUpdate? 'Update Note':'Add Note',style: TextStyle(color: Colors.green),)),
+                ),
+                SizedBox(width: 11),
+                Expanded(
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.all(15),
+                          side: BorderSide(
+                              width: 1
+                          )
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }, child: Text('Cancel',style: TextStyle(color: Colors.red),)),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
